8681d8e6158d3c79c275f0ebf22850e07ba3e182
frameur <60637865+frameur@users.noreply.github.com>
2024-03-09 20:49:06 +0100

    ajout page tableau

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Tableaubrewers from './pages/Tableaubrewers'
 import Historybeer from './pages/Historybeer'
 import Home from './pages/Home'
 
@@ -8,8 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        
+        <Route path="/home" element={<Home />} />
+        <Route path="/tableau" element={<Tableaubrewers />} />
         <Route path="/histoire" element={<Historybeer />} />
       </Routes>
     </BrowserRouter>
