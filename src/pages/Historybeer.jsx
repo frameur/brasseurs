4d0c61710f465e210950a77970e77782c163b8c4
frameur <60637865+frameur@users.noreply.github.com>
2024-05-03 21:55:45 +0200

    erreur eslint

--- src/pages/Historybeer.jsx
+++ src/pages/Historybeer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Navigation from "../components/Navigation";
 import "./history.css";
 import img1 from "./images/introduction.jpg";
